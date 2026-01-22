1019170ea27d35c5a785d0c2e73c939b472edfa8
Pedro Henrique Santana <santana_pedro@discente.ufg.br>
2026-01-22 16:19:16 -0300

    Add root GET endpoint for API status check
    Added a root GET endpoint to check API status.

--- main.py
+++ main.py
@@ -2,13 +2,21 @@ from fastapi import FastAPI
 from moldes import MensagemRecebida, ClassificacaoMensagem
 from services import classificar_mensagem
 
+#Configuração da API FastAPI
 app = FastAPI(
     title="Classificador de Mensagens SAC",
     description="API para a classificação de mensagens"
 )
 
+# Endpoint GET para verificar se a API está funcionando
+@app.get("/")
+def read_root():
+    return {"message": "Bem-vindo ao Classificador de Mensagens SAC"}
+
+# Endpoint POST para classificar mensagens
 @app.post("/classificar", response_model=ClassificacaoMensagem)
 def classificar(mensagem: MensagemRecebida):
     categoria = classificar_mensagem(mensagem.texto)
     return ClassificacaoMensagem(categoria=categoria)
+
 
